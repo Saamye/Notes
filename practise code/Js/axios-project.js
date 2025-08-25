a356591de908ca5b01ffa42470e9a9e70982a32e
samxavier1401@gmail.com <samxavier1401@gmail.com>
2025-08-25 20:11:37 +0100

    exercise 1
    adding new functionality

--- practise code/Js/axios-project.js
+++ practise code/Js/axios-project.js
@@ -6,6 +6,21 @@ const themeToggle = document.getElementById("themeToggle");
 const userContainer = document.getElementById("userContainer");
 const loader = document.getElementById("loader");
 
+
+// additional features
+
+const ex1Btn = document.getElementById("ex1Btn");
+const ex2Btn = document.getElementById("ex2Btn");
+const ex3Btn = document.getElementById("ex3Btn");
+const exerciseResult = document.getElementById("exerciseResult");
+
+// add the eventlisteners for the new functionality
+
+ex1Btn.addEventListener("click", exercise1);
+ex2Btn.addEventListener("click", exercise2);
+ex3Btn.addEventListener("click", exercise3);
+
+
 // initialize an array to store the fetched users
 let users = []
 
@@ -22,7 +37,66 @@ const fetchUsers = (count) => {
     axios
         .get(`https://randomuser.me/api/results=${count}`)
         .then((response) => {
+            loader.style.display = "none";
             users = response.data.results;
+            renderUsers(users);
+        });
+        .catch((error) => {
+            loader.style.display = "none";
+            userContainer.innerHTML = "<p> Error fetching data </p>";
+            console.error(error);
         });
 
 }
+
+const filterUsers = () => {
+    let term = searchInput.value.toLowerCase();
+    let filtered = users.filter((u) => {
+
+    })
+};
+
+const toggleTheme = () => {
+
+};
+
+const renderUsers = (list) => {
+    userContainer.innerHTML = "";
+    list.forEach((user) => {
+        let card = document.createElement("div");
+        card.className = "user-card";
+        card.innerHTML = `
+        <img src="${user.picture.medium}" alt="user-picture" />
+        <h3>${user.name.first} ${user.name.last}</h3>
+        <p>${user.location.city} ${user.location.country}</p>
+        <p>${user.dob.age}</p>
+        <p>${user.email}</p>
+        <p>${user.phone}</p>
+
+        `;
+        userContainer.appendChild(card);
+
+
+    });
+};
+
+//Task 8
+
+function exercise1() {
+    userContainer.innerHTML = "";
+    exerciseResult.textContent = "";
+    loader.style.display = "block";
+
+    axios
+        .get("https://randomuser.me/api/?results=5&nat=US")
+        .then((response) => {
+            loader.style.display = "none";
+            users = response.data.result;
+            renderUsers(users);
+        })
+        .catch((error) => {
+            loader.style.display = "none";
+            userContainer.innerHTML = "<p> Error Loading Users </p>";
+            console.log("Exercise 1 Error", error);
+        })
+};
