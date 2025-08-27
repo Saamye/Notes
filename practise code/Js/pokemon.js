b501f963a1f3c4bd0753ef8e440ca330e77459ec
samxavier1401@gmail.com <samxavier1401@gmail.com>
2025-08-27 21:19:50 +0100

    Update pokemon.js

--- practise code/Js/pokemon.js
+++ practise code/Js/pokemon.js
@@ -1,9 +1,23 @@
 async function getPokemon(pokemonName) {
     try {
-        if(typeof pokemonName === "string") {
+        
+        //checking if the user entered a string
+        if (typeof pokemonName === "string") {
+            
+            //trying to fetch the API based on the input
             const URL = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`);
-        } else {throw new Error("Wrong input!")}
+
+            //to check if the response was successfull using the response.ok functionality
+            if(!response.ok) { throw new Error("Was not able to Fetch the API!!!")}
+          
+              
+        } else { throw new Error("Wrong input!") }
+
+
     } catch (error) {
         console.error("Could not connect to the server!..", error)
     }
 }
+
+
+
