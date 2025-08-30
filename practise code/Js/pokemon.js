f56fcb1f93fbfc419c3fe478ce2b829a4bc78f45
samxavier1401@gmail.com <samxavier1401@gmail.com>
2025-08-30 16:00:19 +0100

    Update pokemon.js

--- practise code/Js/pokemon.js
+++ practise code/Js/pokemon.js
@@ -19,15 +19,42 @@ async function getPokemon(pokemonName) {
 
 
 
-            const typesOfPokemon = pokemon.types.map((element) => element.type.name);
-                
-            const sound = Object.entries(pokemon.cries).map((key, value) => {
-                {
-                    
-                }
-            })
+            const sounds = Object.entries(pokemon.cries).map(([key, value]) => {
+                return {
+                    soundType: key,
+                    sound: value
+                };
+            });
 
+            const card = document.createElement("div");
+            card.classList.add("pokemon-card");
 
+            const img = document.createElement("img");
+            img.src = image;
+            card.appendChild(img);
+
+            const pokemonInfo = document.createElement("p");
+            pokemonInfo.style.margin = "5px";
+            pokemonInfo.style.fontSize = "12px";
+            pokemonInfo.innerHTML = `
+                ${name} <br>
+                ${height*0.1}/m <br>
+                ${weight}oz <br>
+                ${types.join(",")}
+            
+            `;
+            card.appendChild(pokemonInfo);
+
+            const soundCont = document.createElement("div");
+            
+            for (let i = 0; i < sounds.length; i++) {
+                const button = document.createElement("button");
+                button.textContent = sounds[i].soundType;
+                button.addEventListener("click", () => {
+                    const cry = new Audio(sounds[i].sound)  
+                    cry.play();
+                })
+            };
 
 
         } else throw new Error("Wrong input!");
